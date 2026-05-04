b2a7e443d224a56f6e6ce6cc68569dd803dca0cf
lucassilva-aiengineer <lucassilva.aiengineer@gmail.com>
2026-05-03 22:32:45 -0300

    Adicionando novas partes

--- topicos_python/loops/for_loops.py
+++ topicos_python/loops/for_loops.py
@@ -1,6 +1,11 @@
 from typing import List
 import time 
 from faker import Faker
+from typing import List, Optional, Tuple, Union
+import random
+
+# Optional, ou um tipo de dados específico ou None 
+# Union, um dos tipos de dados indicados. Exemplo: int, str, bool, float
 
 # Loop for
 
@@ -124,14 +129,105 @@ def teste():
 
     print(faker.company())
 
+
+def topicos()-> None:
+
+
+
+    # def contratar(candidato, contratados: List[Tuple[str, int]]= [])-> None:
+
+        # contratados.append(candidato)
+
+        # return contratados 
+
+        # contratados 
+
+    candidatos = [("João", 100), ("Jonas", 80), ("Joaquim", 15)]
+
+
+    lista_contratatados: List[Tuple[str, int]] = []
+    def contratar(candidato: Tuple[str, int], lista_candidatos: List[Tuple[str, int]]):
+        lista_candidatos.append(candidato)
+
+
+
+    for candidato in candidatos:
+
+        """
+            Caso o candidato tenha uma pontuação 
+        """
+        if candidato[1] <= 80:
+            continue
+        contratar(candidato, lista_contratatados)
+
+
+    def exibir_contratados(contratados_list: List[Tuple[str, int]]):
+
+        # O enumerate transforma um lista de itens em uma lista de tuplas 
+        # onde o índice 0 se trata do índice do valor e o índice 1 se trata 
+        # do item. 
+
+        print("Exibição 1: ")
+        for index, candidato in enumerate(contratados_list):
+
+            print(f"Nome candidato n° {index + 1}: {candidato[0]}")
+            print(f"Pontuação condidato: {candidato[1]}") 
+
+        print("Exibição 2: ")
+        indice = 1
+        for nome, nota in contratados_list:
+            print(f"Índice: {indice}")
+            print(f"Nome: {nome}")
+            print(f"Nota: {nota}")
+
+
+
+    def encontrar_valor()-> None:
+
+        # a palavra chave break interrompe a execução 
+        # do laço de repetição. 
+
+        tentativas = 0
+        for a in range(0, 101):
+            numero = random.randint(0, 9)
+
+            print(numero)
+            if numero == 9:
+                print("Número 9 encontrado!")
+                tentativas += 1
+                break 
+
+
+            tentativas += 1 
+        print(f"Foram necessárias {tentativas} tentativas.")
+
+    # exibir_contratados(lista_contratatados)
+
+    
+    encontrar_valor()
+def teste1():
+
+    for a in range(10):
+        if a == 1:
+            print("Número Igual a A 1")
+            continue # Pulamos esta interação para a próxima.
+
+        print(a)
+
+
+
+
+
+
 def main():
     # imprimir_lista()
     # preencher_lista()
     # modificando_valores()
 
     # teste()
-    ... 
-
+    
+    # teste1()
+    topicos()
 
 if __name__ == '__main__':
     main()
